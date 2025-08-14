0a5a6a8c3c91d4630440bb17d41ef6e7585ca6a5
Dnyaneshwari-More <154124436+Dnyaneshwari-More@users.noreply.github.com>
2025-08-14 12:53:07 +0530

    Add files via upload
    Area of the reactangle

---
+++ AOR.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+    public class AOR {
+    
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter the length of the rectangle:");
+            double l = sc.nextDouble();
+            System.out.println("Enter the breadth of the rectangle :");
+            double b = sc.nextDouble();
+            double area = l*b;
+            System.out.println("Area of the reactangle is "+ area+"sq.cm");
+    }
+}
